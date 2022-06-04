07171841b668c3f81f5ff784e4bafca24e08c11f
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2022-06-04 22:05:25 +0700

    first Commit

---
+++ function.js
@@ -0,0 +1,25 @@
+
+
+
+// CONTOH FUNCTION SEDERHANA 
+function sayhello() {
+    
+    console.log("Hello");
+    console.log("Hay");
+    console.log("HIIIII");
+};
+
+
+// FUNCTINON EKPRESI/ ANONY FUNCTINON 
+var hallo = function() {
+    console.log("Hello");
+};
+
+// FUNCTION ARROW 
+var jeruk = () => {
+    console.log("Pagii");
+};
+
+
+sayhello();
+console.log(hallo);
